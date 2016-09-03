a907cd1f0990a87bea3d880ace37ec7635eefa0a
ayonafamily <ayonafamily@gmail.com>
2016-09-03 03:14:55 -0500

    codigo maquillado

--- tarea-par.js
+++ tarea-par.js
@@ -6,50 +6,56 @@ Prueba a invocarla con varias manos de cartas.
 3. Desarrolla un programa que use la función anterior para puntuar las manos de cartas de dos jugadores
 e imprima por consola al ganador, es decir, al que obtenga mayor puntuación.    */
 var cartas = [{
-palo: "c",
-valor: 3
+    palo: "c",
+    valor: 3
 }, {
-palo: "t",
-valor: 8
+    palo: "t",
+    valor: 8
 }, {
-palo: "p",
-valor: 1
+    palo: "p",
+    valor: 1
 }, {
-palo: "p",
-valor: 5
+    palo: "p",
+    valor: 5
 }, {
-palo: "d",
-valor: 6
+    palo: "d",
+    valor: 6
 }]
+
 function puntua() {
-total = 0;
-for (i = 0; i < cartas.length; i++) {
-total = total + cartas.valor[i];
-console.log(total);
-}
+    total = 0;
+    for (i = 0; i < cartas.length; i++) {
+        total = total + cartas.valor[i];
+        console.log(total);
+    }
 }
 //---------------------------------------------------------------------
-var nombres =[];
-var n1 =prompt("Ingresa el 1ro");
-var n2 =prompt("Ingresa el 2do");
-var n3 =prompt("Ingresa el 3ro");
-var nombres = [n1,n2,n3]
+var nombres = [];
+var n1 = prompt("Ingresa el 1ro");
+var n2 = prompt("Ingresa el 2do");
+var n3 = prompt("Ingresa el 3ro");
+var nombres = [n1, n2, n3]
 alert(nombres);
 alert(nombres[2]);
 //--------------------------------------------------------------------------
-var amigos =["Lucho", "Napo","Miguel","Roberto","Flavio"];
+var amigos = ["Lucho", "Napo", "Miguel", "Roberto", "Flavio"];
 for (var i = 0; i < 5; i++) {
-console.log(amigos[i])
+    console.log(amigos[i])
 }
 //----------------------------------------------------------------
 
-function lista(){
-var palos = ["c","t","p","d"];
-for(i=0;i<palos.length;i++){
-	for (j=1;j<14;j++){
-	console.log(palos[i]+j);
-	}
-	}
+function lista() {
+    var palos = ["c", "t", "p", "d"];
+    for (i = 0; i < palos.length; i++) {
+        for (j = 1; j < 14; j++) {
+            console.log(palos[i] + j);
+        }
+    }
 }
-	//--------------------------------------------------------------------
-	var cartas =[{palo:"c",valor:5},palo:"t",valor:2];
+//--------------------------------------------------------------------
+var cartas = [{
+        palo: "c",
+        valor: 5
+    },
+    palo: "t", valor: 2
+];
