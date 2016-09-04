ff43842d65402372a9b6b96a7a2e1043028c37ee
ayonafamily <ayonafamily@gmail.com>
2016-09-03 23:50:26 -0500

    Agregado puntuar la mano

--- tarea-par.js
+++ tarea-par.js
@@ -6,28 +6,27 @@ Prueba a invocarla con varias manos de cartas.
 3. Desarrolla un programa que use la función anterior para puntuar las manos de cartas de dos jugadores
 e imprima por consola al ganador, es decir, al que obtenga mayor puntuación.    */
 var cartas = [{
-    palo: "c",
-    valor: 3
+palo: "c",
+valor: 3
 }, {
-    palo: "t",
-    valor: 8
+palo: "t",
+valor: 8
 }, {
-    palo: "p",
-    valor: 1
+palo: "p",
+valor: 1
 }, {
-    palo: "p",
-    valor: 5
+palo: "p",
+valor: 5
 }, {
-    palo: "d",
-    valor: 6
+palo: "d",
+valor: 6
 }]
-
 function puntua() {
-    total = 0;
-    for (i = 0; i < cartas.length; i++) {
-        total = total + cartas.valor[i];
-        console.log(total);
-    }
+total = 0;
+for (i = 0; i < cartas.length; i++) {
+total = total + cartas.valor[i];
+console.log(total);
+}
 }
 //---------------------------------------------------------------------
 var nombres = [];
@@ -40,25 +39,59 @@ alert(nombres[2]);
 //--------------------------------------------------------------------------
 var amigos = ["Lucho", "Napo", "Miguel", "Roberto", "Flavio"];
 for (var i = 0; i < 5; i++) {
-    console.log(amigos[i])
+console.log(amigos[i])
 }
 //----------------------------------------------------------------
 /*Función que lista los 52 palos de la baraja dividos por palo   */
-
 function lista() {
-    var palos = ["c", "t", "p", "d"];
-    for (i = 0; i < palos.length; i++) {
-        for (j = 1; j < 14; j++) {
-            console.log(palos[i] + j);
-        }
-    }
+var palos = ["c", "t", "p", "d"];
+for (i = 0; i < palos.length; i++) {
+for (j = 1; j < 14; j++) {
+console.log(palos[i] + j);
+}
+}
 }
 //--------------------------------------------------------------------
 croupier = {};
-
 var cartas = [{
-        palo: "c",
-        valor: 5
-    },
-    palo: "t", valor: 2
+palo: "c",
+valor: 5
+},
+palo: "t", valor: 2
 ];
+/* Inseción de un valor en un array */
+var cartas = ["5d", "3t", "2c", "12c"];
+var resultado = [];
+for (var i = 0; i < cartas.length; i++) {
+resultado[resultado.length] = cartas[i];
+if (cartas[i] === "3t") {
+resultado[resultado.length] = "1c";
+}
+}
+// Encontrar  un elemento en un array. En este caso: Barajas rojas
+var cartas = ["5d", "3t", "2c", "12c"];
+var resultado = [];
+for (i = 0; i < cartas.length; i++) {
+if (cartas[i][1] === "d" || cartas[i][1] === "c") {
+resultado = cartas[i];
+console.log(resultado);
+}
+}
+//Búsqueda en array
+var cartas = ["5d", "3t", "2c", "12c"];
+var resultado = [];
+for (i = 0; i < cartas.length; i++) {
+if (cartas[i] === "2c") {
+encontrado = true;
+resultado = [cartas[i]]
+console.log(encontrado + " " + resultado)
+}
+}
+/*llamada a una función */
+function puntua(mano = [10, 9, 8, 7, 8]) {
+var total = 0;
+for(var i=0;i<mano.length;i++){
+total=total+mano[i];
+}
+console.log(total);
+}
